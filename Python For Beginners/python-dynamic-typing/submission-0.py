105a91f5b94c5dd19036814e2389970100f98fe4
Lars <goozenlars@gmail.com>
2026-08-05 02:07:38 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 8
+print(type(variable))
+variable = 8.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "True"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
